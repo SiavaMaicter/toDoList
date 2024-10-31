2d37f8db003ac31746591f71323b98984527937f
SiavaMaicter <settelelyol@gmail.com>
2024-10-31 14:24:59 +0200

    show todo list

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,11 +1,48 @@
 <template>
   <div>APP PAGE</div>
+  <div>
+    <div class="table-responsive">
+      <table
+        class="table table-striped table-hover table-borderless table-primary align-middle"
+      >
+        <thead class="table-light">
+          <caption>
+            TODO LIST
+          </caption>
+          <tr>
+            <th>id</th>
+            <th>name</th>
+            <th>description</th>
+          </tr>
+        </thead>
+        <tbody class="table-group-divider">
+          <tr v-for="todo in todos" :key="todo.id">
+            <td>
+              {{ todo.id }}
+            </td>
+            <td>
+              {{ todo.name }}
+            </td>
+            <td>
+              {{ todo.description }}
+            </td>
+          </tr>
+        </tbody>
+        <tfoot></tfoot>
+      </table>
+    </div>
+  </div>
   <!-- <router-view></router-view> -->
 </template>
 
 <script>
 import axios from "axios";
 export default {
+  data() {
+    return {
+      todos: null,
+    };
+  },
   mounted() {
     axios.get("/api/todos").then((res) => {
       console.log(res.data);
