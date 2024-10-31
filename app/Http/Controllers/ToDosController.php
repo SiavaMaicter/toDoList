32bcd38b45e44bb797ff782b53aa13853b1cff3a
SiavaMaicter <settelelyol@gmail.com>
2024-10-31 15:04:16 +0200

    custom states

--- app/Http/Controllers/ToDosController.php
+++ app/Http/Controllers/ToDosController.php
@@ -3,13 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Models\ToDo;
+use Error;
 use Illuminate\Http\Request;
 
 class ToDosController extends Controller
 {
     public function index()
     {
-        $todos = ToDoвфвфв::all();
-        return compact('todos');
+        try {
+            $todos = ToDo::all();
+        } catch (Error $err) {
+            // return response()->json(["error" => "Failed to retrieve tasks", "status" => 500]);
+            return response("Failed to retrieve tasks", 500);
+        }
+        return response(compact('todos'), 200);
+        // return response()->json([compact('todos'), "status" => 500]);
     }
 }
