83d6aa553d455555e81a127a70046f122a209e0b
SiavaMaicter <settelelyol@gmail.com>
2024-11-02 13:01:16 +0200

    enable bootstrap

--- resources/js/components/CreatePage.vue
+++ resources/js/components/CreatePage.vue
@@ -2,7 +2,12 @@
   <title>CREATE PAGE</title>
   <div class="mb-3">
     <label for="" class="form-label">Name</label>
-    <input type="text" class="form-control" aria-describedby="helpName" />
+    <input
+      type="text"
+      class="form-control"
+      aria-describedby="helpName"
+      v-model="todo.name"
+    />
     <small id="helpName" class="form-text text-muted">String name</small>
   </div>
   <div class="mb-3">
@@ -11,13 +16,21 @@
       type="text"
       class="form-control"
       aria-describedby="helpDescription"
+      v-model="todo.description"
     />
     <small id="helpDescription" class="form-text text-muted">String text</small>
   </div>
+  <button class="btn btn-primary">SUBMIT</button>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      todo: {},
+    };
+  },
+};
 </script>
 
 <style>
